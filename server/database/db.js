b7c642fd61373bfc5b87726e74f122bbbbb5700f
Andrew Klapper <ak475826@gmail.com>
2024-01-10 13:50:39 -0500

    try dotenv fix 1

--- server/database/db.js
+++ server/database/db.js
@@ -8,7 +8,9 @@ const URL = process.env.DB_URL;
 
 console.log(URL);
 
-const mongoClient = new MongoClient(process.env.DB_URL);
+const mongoClient = new MongoClient(
+	'mongodb+srv://aKlapper:ogvD6BJq4AJiUPGY@cluster0.dehexay.mongodb.net/?retryWrites=true&w=majority'
+);
 
 const DB_NAME = process.env.DB_NAME;
 const DB_COLLECTION = process.env.DB_COLLECTION;
