fe86fbc944415f1e22dcdce47e4d4f63d871752a
Andrew Klapper <ak475826@gmail.com>
2024-01-10 15:15:22 -0500

    fix websocket client url1

--- client/src/socket.io.js
+++ client/src/socket.io.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = 'https://login-server-131l.onrender.com:4200';
+const URL = 'http://login-server-131l.onrender.com:4200';
 
 const socket = io(URL, { autoConnect: false });
 
