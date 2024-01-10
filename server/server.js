047121d9692216a4ad1abc47d8544b5ac87cf803
Andrew Klapper <ak475826@gmail.com>
2024-01-10 15:05:20 -0500

    fix websocket client url1

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import httpServer from './websockets/socket.io.js';
-
+import cors from 'cors';
 const PORT = process.env.PORT;
 
+httpServer.use(cors());
 httpServer.listen(PORT, () => {
 	console.log(`httpServer active on ${PORT}`);
 });
