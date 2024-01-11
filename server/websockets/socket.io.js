02ecc3a3529f9754275a851df92ee674169e65fe
Andrew Klapper <ak475826@gmail.com>
2024-01-11 12:32:39 -0500

    updated ssl cert

--- server/websockets/socket.io.js
+++ server/websockets/socket.io.js
@@ -16,7 +16,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
 	cors: {
-		origin: 'https://login-server-131l.onrender.com',
+		origin: 'https://andrew-k.us',
 	},
 });
 
