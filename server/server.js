5b2e5ff7d732cf2f43c8c146135724e64d2934bc
Andrew Klapper <ak475826@gmail.com>
2024-01-12 12:52:40 -0500

    change server password hash

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ import httpServer from './websockets/socket.io.js';
 
 const PORT = process.env.PORT || 4200;
 
-httpServer.listen(PORT, '127.0.0.1', () => {
+httpServer.listen(PORT, () => {
 	console.log(`httpServer active on ${PORT}`);
 });
