3b934a0ff8807e8d68565a4236862e34bc121b55
Andrew Klapper <ak475826@gmail.com>
2024-01-18 22:17:17 -0500

    tweaked session

--- server/src/controllers/session_user_login.js
+++ server/src/controllers/session_user_login.js
@@ -15,11 +15,10 @@ export default async function sessionUserLogin(req, res, next) {
 			await res.send({ email: email, authorized: authorized });
 			next();
 		} else {
-			res.send('/login');
 			next();
 		}
 	} else {
-		res.send({ message: 'no session in db' }).status(200);
+		res.send('/login').status(200);
 		next();
 	}
 }
