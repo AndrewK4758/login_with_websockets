2c52ac38a037745b74491aacf8fabdb44dc0951e
Andrew Klapper <ak475826@gmail.com>
2024-01-18 23:45:20 -0500

    changed headers forcookie / session

--- server/app.js
+++ server/app.js
@@ -10,17 +10,24 @@ import { router as loginUser } from './src/routes/login_user.js';
 import { router as registerUser } from './src/routes/register_user.js';
 import { router as sessionUserLogin } from './src/routes/session_user_login.js';
 import { router as logoutUser } from './src/routes/logout_user.js';
-
+/*
+{
+		origin: ['andrew-k.us', 'www.andrew-k.us'],
+		methods: ['POST', 'GET', 'PATCH', 'PUT', 'DELETE'],
+		credentials: true,
+		allowedHeaders: ['A'],
+		exposedHeaders: ['Set-Cookie'],
+	}
+*/
 app.use(sessionData);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
 	cors({
-		origin: ['*'],
-		methods: ['POST', 'GET', 'PATCH', 'PUT', 'DELETE'],
-		credentials: true,
-		allowedHeaders: ['*'],
-		exposedHeaders: ['Set-Cookie'],
+		origin: '*',
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		preflightContinue: false,
+		optionsSuccessStatus: 204,
 	})
 );
 
